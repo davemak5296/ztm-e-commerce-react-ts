bfcc1ac01e093cc8af9c957caa36c96f08cb4276
Dave Mak <mak9652615@gmail.com>
2022-07-06 18:05:09 +0800

    add redirect:redirect /auth to / if there is active user session

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { Route, Routes } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { Route, Routes, Navigate } from 'react-router-dom';
 import Authentication from './routes/authentication.component';
 import Cart from './routes/cart.component';
 import Home from './routes/home.component';
 import Navigation from './routes/navbar.component';
 import Shop from './routes/shop.component';
+import { selectCurrentUser } from './store/user/user.selector';
 
 const App = () => {
   const dispatch = useDispatch();
+  const currUser = useSelector(selectCurrentUser);
 
   useEffect(() => {
     dispatch({ type: 'user/CHECK_USER_SESSION' });
@@ -19,7 +21,7 @@ const App = () => {
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
         <Route path="shop/*" element={<Shop />} />
-        <Route path="auth" element={<Authentication />} />
+        <Route path="auth" element={currUser ? <Navigate to="/" /> : <Authentication />} />
         <Route path="cart" element={<Cart />} />
       </Route>
     </Routes>
